15419bbd952182116f77d2209a0b0dc833975f17
vira <156062320+ViraKrajevskiy@users.noreply.github.com>
2024-12-17 18:53:06 +0500

    Save local changes before pulling updates

--- card.py
+++ card.py
@@ -1,4 +1,9 @@
-class Card():
-    def __init__(self,number,password,phone,datework):
-        self.number = number
-        self.pass
+class Card:
+    def __init__(self,number,password,datework,balance):
+        self.number = []
+        self.password = password
+        self.datework = datework
+        self.balance = 0
+
+    def info(self):
+        return f"{self.number,self.password,self.datework,self.balance}"
--- user.py
+++ user.py
@@ -1,9 +1,25 @@
 from card import Card
 
-class User(Card):
-    def __init__(self,name,surname,bag,admin):
-        super().__init__()
-        self.name = name
-        self.surname = surname
-        self.bag = bag
-        self.admin = admin
+class User:
+    def __init__(self,namesurname,phone,cards,basket,role,passportId):
+        self.namesurname = namesurname
+        self.phone = phone
+        self.basket = []
+        self.role = role
+        self.passportId = passportId
+        self.cards = []
+        # if self.basket == None:
+        #    print("Пока нету продуктов")
+        # else:
+        #     print(self.basket)
+
+
+    def info(self):
+        return f"Имя пользователя и фамилия: {self.namesurname}\nНомер телефона: { self.phone}\n Айди пользователя: {self.passportId}\nПродукты в карзинепользовтеля: {self.basket}"
+
+card1 = Card(8965420447844521,7346,"07/25",0)
+
+user1 = User("Вира Кражевский",900368052,None,None,"User","AD0452765",)
+user1.cards.append(card1)
+
+print(user1.info())
